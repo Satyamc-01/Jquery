77e34574c7d55813452aea2778c620d9492c73a6
Satyamc_01 <satyamchaudhary5523@gmail.com>
2022-04-28 20:20:20 +0530

    April 28 Addmore

--- Task-7/task1/js/addmore.js
+++ Task-7/task1/js/addmore.js
@@ -1,83 +1,109 @@
 $(document).ready(function () {
-    $("#addmore").click(function () {
 
-        // count++
-        $(".block1").append(`
+
+    $(".details").append(`
+    <table class="tab1">
+    <thead>
+    <tr></tr>
+    </thead>
+    <tbody>
+    </tbody>
+    </table>`)
+
+    $(".addmore").click(function () {
+        bootbox.confirm("Do you want to add", function (result) {
+            if (result == true) {
+                $(".block1").append(`
         <div class="block clearfix">
             <div class="inside-btn fl">
-                <input type="text" id="title" placeholder="Title">
-                <button type="button" id="addchild">Add Child</button>
-                <button type="button" id="submit">Submit</button>                        <button type="button" id="delete">Delete</button>
+                <input type="text" class="title" placeholder="Title">
+                <button type="button" class="addchild">Add Child</button>
+                <button type="button" class="submit">Submit</button>
+                <button type="button" class="delete">Delete</button>
             </div>
         </div>
         `)
-        // console.log(count);
-    })
 
-    $(".block1").on("click", "#addchild", function () {
-        pos = $(this).parent().parent().index()
-        $(".block").eq(pos).append(`
-            <div class="child fl">
-            <input type="text" id="subtitle" placeholder="Subtitle">
-            <input type="text" id="value" placeholder="Value">
-            <button type="button" id="delete1">Delete</button>
-        </div>
-        `)
-    })
-    $("body").on("click", "#delete", function () {
-        $(this).parent().parent().remove()
-
-
-
-    })
-    $(".block1").on("click", "#delete1", function () {
-        $(this).parent().remove()
-    })
-
-    $(".block1").on("click", "#submit", function () {
-        var srno = $(this).parent().parent().index();
-        var a = $(this).siblings().eq(0).val()//parentval
-        var b = $(this).parent().siblings().length//childlength
-
-        if ($(`#${srno}`).length == 0) {
-
-            $(this).parents().find(".details").append(`
-     
-        <table id="${srno}">
+                $(".details").append(`
+        
+        <table class="tab1">
         <thead>
-        <tr><th colspan=2>${a}</th></tr>
+        <tr></tr>
         </thead>
+        <tbody>
+        </tbody>
         </table>`)
+            }
+        });
 
-        }
 
-        $(this).parents().find(`table#${srno}`).find("thead").remove()
-        $(this).parents().find(`table#${srno}`).prepend(`
-  
-        <thead class="head1">
-        <tr><th colspan=2>${a}</th></tr>
-        </thead>
-     
+
+    })
+
+    $("body").on("click", ".addchild", function () {
+        var xa = this
+        bootbox.confirm("You want to add subtitle?", function (result) {
+            if (result == true) {
+                var position = $(".addchild").index(xa)
+
+                $(".inside-btn").eq(position).append(`
+                    <div class="child fl">
+                    <input type="text" class="subtitle" placeholder="Subtitle">
+                    <input type="text" class="value" placeholder="Value">
+                    <button type="button" class="delete1">Delete</button>
+                </div>
+                `)
+            }
+
+        });
+    })
+
+    $("body").on("click", ".delete", function () {
+        var aaa=this
+        bootbox.confirm("You want to delete??", function (result) {
+            if (result == true) {
+                var delindex = $(".delete").index(aaa)
+                $(".block").eq(delindex).remove()
+                $(".tab1").eq(delindex).remove()
+            }
+
+        });
+
+
+    })
+    $(".block1").on("click", ".delete1", function () {
+        bootbox.confirm("You want to delete??", function (result) {
+            if (result == true) {
+                var childdlt = $(".delete1").index(this)
+                $(".child").eq(childdlt).remove()
+
+            }
+
+        });
+    })
+
+    $("body").on("click", ".submit", function () {
+
+        var pos = $(".submit").index(this);
+        var b = $(this).parents(".block").find(".child").length//childlength
+        $("table").eq(pos).find("th").remove()
+        $("table").eq(pos).find("tbody").empty()
+        var a = $(this).parents(".block").find(".title").val()
+        $(this).parents().find("table thead tr").eq(pos).append(`
+        <th colspan=2>${a}</th>
         `)
-        $(this).parents().find(`table#${srno}`).find("tbody").remove()
-
-        console.log(srno,a);
-        console.log(b);
         for (i = 0; i < b; i++) {
-
-
-            var subtitle = $(this).parent().siblings().eq(i).children().eq(0).val()
-            var value = $(this).parent().siblings().eq(i).children().eq(1).val()
-            // console.log(subtitle, value);
-            $(this).parents().children().find(`.details table#${srno}`).append(`<tbody><tr>
-            
+            var subtitle = $(this).parents(".block").find(".subtitle").eq(i).val()
+            var value = $(this).parents(".block").find(".value").eq(i).val()
+            console.log(subtitle, value);
+            $("table").eq(pos).find("tbody").append(`<tr>
                 <td>${subtitle}</td>
                 <td>${value}</td>
-            
-                </tr></tbody> `)
+                </tr> `)
 
-            // console.log(srno, a);
         }
 
+
     })
+
 });
